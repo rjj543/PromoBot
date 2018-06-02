eff445b5aa5499690cb20b3c05f13b26d9c7e8cd
rjj543 <flashmaker24@gmail.com>
2018-06-01 20:43:06 -0400

    Create index.js

---
+++ index.js
@@ -0,0 +1,38 @@
+const prefix = "!"
+const Discord = require('discord.js');
+const bot = new Discord.Client();
+
+
+
+bot.on('message', (message) => {
+   
+    if(message.content == prefix + 'Channel') {
+        message.channel.sendMessage('https://www.youtube.com/channel/UChXNvQj5mw-vJmC_Lv3f10Q');
+    }
+});
+
+bot.on('message', (message) => {
+   
+    if(message.content == prefix + 'SecondChannel') {
+        message.channel.sendMessage('https://www.youtube.com/channel/UCbjZGpSgIMbfdw5OXPzJvbQ');
+    }
+});
+
+bot.on('message', (message) => {
+   
+    if(message.content == prefix + 'Cmds') {
+        message.channel.sendMessage('```Channel``` ```SecondChannel``` ```Prefix.```');
+    }
+});
+
+bot.on('message', (message) => {
+   
+    if(message.content == 'Prefix') {
+        message.channel.sendMessage('The current prefix is: !');
+    }
+});
+
+
+
+bot.login(process.env.BOT_TOKEN);
+// yeet it up B
